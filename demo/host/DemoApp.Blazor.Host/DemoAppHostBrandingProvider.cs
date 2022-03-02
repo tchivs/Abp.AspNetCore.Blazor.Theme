67e553a1ae7a4a72b62d33106d50cd8a4f950311
luocheng <cheng.luo2@hytera.com>
2022-03-02 17:52:36 +0800

    update blazor brandingProvider

--- demo/host/DemoApp.Blazor.Host/DemoAppHostBrandingProvider.cs
+++ demo/host/DemoApp.Blazor.Host/DemoAppHostBrandingProvider.cs
@@ -1,8 +1,10 @@
-﻿using Volo.Abp.Ui.Branding;
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
 
 namespace DemoApp.Blazor.Host;
+[Dependency(ReplaceServices = true)]
 
 public class DemoAppHostBrandingProvider : DefaultBrandingProvider
 {
-    public override string AppName => "DemoApp";
+    public override string AppName => "DemoApp|wasm";
 }
